5b855c82435a8545fa47f4ca9d0b3bad41aa04ad
IT21388934 <it21388934@my.sliit.lk>
2023-12-19 13:34:19 +0530

    implement preloading animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,20 +8,36 @@ import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  }, [isLoading]);
 
   return (
     <>
       {/* <div className="mainContainer"></div> */}
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/about" element={<AboutScreen />} />
-        </Routes>
-      </BrowserRouter>
+      {isLoading ? (
+        <div className="loadingContainer">
+          <HashLoader color="#5ce6d4" size={50} isLoading={isLoading} />
+        </div>
+      ) : (
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/about" element={<AboutScreen />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </>
   );
 }
